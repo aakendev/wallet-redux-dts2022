4e3ed583a9fa16025eda3cb25024b7b7c56ce217
Aldy Rizky Putra <aakendev@gmail.com>
2022-07-19 14:05:51 +0700

    Adding Redux Middleware

--- src/containers/Wallet.js
+++ src/containers/Wallet.js
@@ -1,32 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { deposit, selectUser, selectBalance, withdraw } from '../reducers/walletSlice';
+import { deposit, selectUser, selectBalance, withdraw, userAsync } from '../reducers/walletSlice';
 
 const Wallet = () => {
   const user = useSelector(selectUser);
   const balance = useSelector(selectBalance);
   
-  const [currentAmount, setCurrentAmount] = useState();
+  const [currentAmount, setCurrentAmount] = useState(0);
+  const [currentId, setCurrentId] = useState();
   
   const dispatch = useDispatch();
   
-  const handleWithdraw = (amount) => {
-    dispatch(withdraw(amount));
-  }
-  const handleDeposit = (amount) => {
-    dispatch(deposit(amount));
+  useEffect(() => {
+    dispatch(userAsync(4));
+  }, [])
+  
+  const handleUserClickId = () => {
+    dispatch(userAsync(currentId));
   }
   
   return (
-    <div>
-      <h1>{user} Wallet</h1>
+    <div style={{ marginTop: '20px' }}>
+      <img src={user.avatar} alt='avatar' />
+      <h1>{user.first_name} Wallet</h1>
+      <input type='number' onChange={(e) => setCurrentId(e.target.value)}/>
+      <button onClick={() => handleUserClickId()}>Get User Id</button>
       <h2>Balance: Rp {balance.toLocaleString('ID')}</h2>
-      <button onClick={() => handleWithdraw(10000)}>Withdraw Rp 10.000</button>
-      <button onClick={() => handleDeposit(10000)}>Deposit Rp 10.000</button>
+      <button onClick={() => dispatch(withdraw(10000))}>Withdraw Rp 10.000</button>
+      <button onClick={() => dispatch(deposit(10000))}>Deposit Rp 10.000</button>
       <br />
       <input type="number" onChange={(e) => setCurrentAmount(parseInt(e.target.value))} />
-      <button onClick={() => handleWithdraw(currentAmount)}>Withdraw</button>
-      <button onClick={() => handleDeposit(currentAmount)}>Deposit</button>
+      <button onClick={() => dispatch(withdraw(currentAmount))}>Withdraw</button>
+      <button onClick={() => dispatch(deposit(currentAmount))}>Deposit</button>
     </div>
     )
 }
--- src/reducers/walletSlice.js
+++ src/reducers/walletSlice.js
@@ -1,6 +1,21 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-const initialState = { user: 'Aldy', balance: 500000 };
+const initialState = { user: {
+  id: 0,
+  email: "",
+  first_name: "",
+  last_name: "",
+  avatar: ""
+}, balance: 500000 };
+
+export const userAsync = createAsyncThunk(
+  'wallet/fetchUser',
+  async (id) => {
+    const response = await axios.get(`https://reqres.in/api/users/${id}`);
+    return response.data.data;
+  }
+)
 
 const walletSlice = createSlice({
   name: "wallet",
@@ -13,7 +28,19 @@ const walletSlice = createSlice({
       if(state.balance >= action.payload){
         state.balance -= action.payload;
       }
-    }
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(userAsync.fulfilled, (state, action) => {
+        state.user = action.payload;
+      })
+      .addCase(userAsync.pending, () => {
+        console.log('loading');
+      })
+      .addCase(userAsync.rejected, () => {
+        console.log('fail to get user');
+      })
   }
 });
 
